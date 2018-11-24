58b07211023b734c985b58a2740834772ca30048
seokju-na <seokju.me@gmail.com>
2018-11-25 03:33:29 +0900

    #3 DB 모델 추가

---
+++ app/models/bottle.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+
+const Bottle = db.define('Bottle', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    createdTimestamp: {
+        type: Sequelize.INTEGER,
+    },
+    hits: {
+        type: Sequelize.INTEGER,
+    },
+});
+
+
+module.exports = Bottle;
