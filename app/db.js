99c30167bcd4f1e6a68f049203927ad44aa9544f
seokju-na <seokju.me@gmail.com>
2019-01-10 22:59:29 +0900

    이전 프로젝트 삭제

--- app/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const environment = require('../environment');
-
-
-const db = new Sequelize(environment.config.DB_URL);
-
-module.exports = db;
--- configs/example.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: 5000,
-    DB_URL: 'mysql://localhost:'
-};
